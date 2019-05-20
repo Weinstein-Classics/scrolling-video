98fcdb09415ac7dc8a901607de5229b94c989bc1
Ivar Rydstrom <45040436+BlizardWizard@users.noreply.github.com>
2019-05-20 14:41:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def root_page():
   return """
-  <video >
-    <source src="officer.mp4"/>
+  <video id="vid" width="100%" style="top:0; left:0; position:fixed">
+    <source src="officer.mp4" type="video/mp4"/>
   </video>
   
   

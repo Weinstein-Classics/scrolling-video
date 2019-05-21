4c63e0d5ca5a8bf4491732566444e61a15a14ed5
Kabir Kwatra <kabir@kwatra.me>
2019-05-21 10:15:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,40 @@ app = Flask(__name__, static_url_path='')
 # root url route
 @app.route("/")
 def root_page():
-  return app.send_static_file('index.html')
+  return """<!DOCTYPE html>
+
+<html>
+
+  <head>
+  	<meta content="width=device-width, inital-scale=1.0"/>
+	<link rel="stylesheet" href="main.css"/>
+  </head>
+
+
+  <body id="main">
+  	<section class="container">
+  		<div class="content">
+  			<h1>Scroll pls</h1>
+  			<p1>start</p1>
+  		</div>
+  	</section>
+
+    <video id="vid" tabindex="0" autobuffer preload width="100%" style="top:0; left:0; position:fixed">
+      <source src="officer.mp4" type='video/mp4;codecs="avc1.42E01E, mp4a.40.2"'/>
+    </video>
+
+    <div id="buffer" height="9999" width="100%" style="margin-top:9999px">hello world</div>
+
+
+
+  </body>
+
+
+
+
+</html>"""
+
+#app.send_static_file('index.html')
 
 """
   <video id="vid" width="100%" style="top:0; left:0; position:fixed">

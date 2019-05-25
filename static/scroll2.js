ec7fdc1e253569abcaad49f62ee44403e93a3476
Kabir Kwatra <kabir@kwatra.me>
2019-05-25 11:21:08 -0700

    Update scroll2.js

--- static/scroll2.js
+++ static/scroll2.js
@@ -1,17 +1,17 @@
 var frameNumber = 0;
-var playConst = 1000;
+var playConst = 500;
 var setHeight = document.getElementById("set-height");
 var vid = document.getElementById("video");
 
 vid.addEventListener("loadedmetadata", function() {
-  setHeight.style.height = Math.floor(vid.duration) + playConst + "px";
+	setHeight.style.height = Math.floor(vid.duration) + playConst + "px";
 });
 
 
-play = function() {
-  var frameNumber = window.pageYOffset / playConst;
-  vid.currentTime = frameNumber;
-  window.requestAnimationFrame(play);
+function play() {
+	var frameNumber = window.pageYOffset / playConst;
+	vid.currentTime = frameNumber;
+	window.requestAnimationFrame(play);
 };
 
 window.requestAnimationFrame(play);

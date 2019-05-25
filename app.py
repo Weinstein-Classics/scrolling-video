f75d564c0f749a620de5742b6f7fd5026a5a1d9b
Kabir Kwatra <kabir@kwatra.me>
2019-05-24 18:02:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 import flask
 app = Flask(__name__, static_url_path='/static')
 
+"""
 @app.route('/static/<path:path>')
 def send_static(path):
     return send_from_directory('/static', path)
+"""
 
 # root url route
 @app.route("/")

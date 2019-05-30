08fbad83f13f4781a1291ef6a87d86f799e6b917
Ivar Rydstrom <45040436+BlizardWizard@users.noreply.github.com>
2019-05-30 13:54:21 -0700

    Update scroll2.js

--- static/scroll2.js
+++ static/scroll2.js
@@ -14,7 +14,7 @@ function play() {
 	var frameNumber = window.pageYOffset / playConst;
 	vid.currentTime = frameNumber;
 	window.requestAnimationFrame(play);
-	if (vid.currentTime >= vid.duration - 2) {
+	if (vid.currentTime >= vid.duration - 10) {
 		vid.currentTime = 0;
 		
 	};

788ea3b0f1d9a1bf5088d175ecbcfecab2d217a3
Kabir Kwatra <kabir@kwatra.me>
2019-05-30 13:33:30 -0700

    Update scroll2.js

--- static/scroll2.js
+++ static/scroll2.js
@@ -3,6 +3,8 @@ var playConst = 500;
 var setHeight = document.getElementById("set-height");
 var vid = document.getElementById("video");
 
+vid.playbackRate = 1;
+
 vid.addEventListener("loadedmetadata", function() {
 	setHeight.style.height = Math.floor(vid.duration) * playConst + "px";
 });

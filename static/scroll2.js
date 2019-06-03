76a8a6369191ec8d0802e95a68f18e721836e0fd
Kabir Kwatra <kabir@kwatra.me>
2019-06-02 22:56:31 -0700

    Update scroll2.js

--- static/scroll2.js
+++ static/scroll2.js
@@ -2,13 +2,12 @@ var playConst = 500;
 var unknownConst = 300
 var setHeight = document.getElementById("set-height");
 var vid = document.getElementById("video");
-var vidEnd = 0;
+var vidEnd = vid.duration - 2;
 vid.playbackRate = 1;
 
 
 vid.addEventListener("loadedmetadata", function() {
 	setHeight.style.height = Math.ceil(vid.duration) * playConst + "px";
-	vidEnd = vid.duration - 2;
 	window.requestAnimationFrame(play);
 });
 
@@ -18,10 +17,8 @@ function play() {
 	vid.currentTime = frameNumber;
 	console.log(frameNumber);
 	if (vid.currentTime >= vidEnd) {
-		window.pageYOffset = 0;
-		console.log("end");
 		window.scrollTo(0,0);
-		vid.frameNumber = 0;
+		frameNumber = 0;
 		vid.currentTime = 0;
 	};
 	

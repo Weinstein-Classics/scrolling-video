441f06a88049336fd4d28a5e0709ed7416a83725
Kabir Kwatra <kabir@kwatra.me>
2019-06-03 14:39:44 -0700

    Added Docs

--- static/scroll2.js
+++ static/scroll2.js
@@ -1,4 +1,4 @@
-var playConst = 500;
+var playConst = 500; // Scrolling Sensitivity
 var setHeight = document.getElementById("set-height");
 var vid = document.getElementById("video");
 var vidEnd = 0;
@@ -6,20 +6,20 @@ vid.playbackRate = 1;
 
 
 vid.addEventListener("loadedmetadata", function() {
-	setHeight.style.height = Math.ceil(vid.duration) * playConst + playConst + "px";
-	vidEnd = vid.duration;
-	window.requestAnimationFrame(play);
+	setHeight.style.height = Math.ceil(vid.duration) * playConst + playConst + "px"; // sets height
+	vidEnd = vid.duration; // sets the end of video
+	window.requestAnimationFrame(play); // adds play function to run each frame
 });
 
 
 function play() {
-	vid.currentTime = window.pageYOffset / playConst;
-	if (vid.currentTime >= vidEnd) {
-		window.scrollTo(0,0);
-		vid.currentTime = 0;
+	vid.currentTime = window.pageYOffset / playConst; // sets time to window y pos
+	if (vid.currentTime >= vidEnd) { // end of video
+		window.scrollTo(0,0); // move to top of screen
+		vid.currentTime = 0; // reset video time to 0
 	};
 	
-	window.requestAnimationFrame(play);
+	window.requestAnimationFrame(play); // recursive call
 };
 
 
